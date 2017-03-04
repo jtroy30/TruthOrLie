222e5cf1a98493fc83dec7747f46bdafe81dcf87
dongy7 <dongy7@gmail.com>
2017-03-04 11:41:39 -0500

    add navigator

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Navigator } from 'react-native';
+import StartScreen from './StartScreen';
+import TopicScreen from './TopicScreen';
+
+class App extends Component {
+  render() {
+    return (
+      <Navigator
+        style={{ flex: 1 }}
+        initialRoute={{ name: 'StartScreen '}}
+        renderScene={this.renderScene}
+      />
+    );
+  }
+
+  renderScene(route, navigator) {
+    if (route.name === 'Start') {
+      return <StartScreen navigator={navigator} />
+    } else if (route.name === 'Topic') {
+      return <TopicScreen navigator={navigator} />
+    }
+  }
+}
+
+export default App;
